6ea7fbc2d5d50206d79b7bf9ed6e23ffa409004a
bellandry <bellandry.work@gmail.com>
2024-11-25 20:24:01 +0100

    add icon & allow hygraph images

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: { unoptimized: true },
-// };
 
-// module.exports = nextConfig;
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'eu-west-2.graphassets.com',
+        port: '', // Leave empty if no custom port
+        pathname: '/**', // Match all paths
+      },
+    ],
+  },
   trailingSlash: false, // Set based on your hosting preferences
   basePath: "", // Ensure this matches the production deployment path
 };
